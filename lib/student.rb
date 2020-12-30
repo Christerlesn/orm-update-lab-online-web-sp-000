21299b56a8d836bace3aa41ec188e305c49a9393
Christerle Nounoute <christerlesn@gmail.com>
2020-12-30 00:42:01 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -46,8 +46,8 @@ class Student
   end
 
   def self.new_from_db(row)
-    student = self.new(name, grade)
-    #student.id = row[0]
+    student = self.new(name, grade, id)
+    student.id = row[0]
     student.name = row[1]
     student.grade = row[2]
   end
