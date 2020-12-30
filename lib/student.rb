21cdcec5d19a7089fb0629a41010eac35efa52e6
Christerle Nounoute <christerlesn@gmail.com>
2020-12-30 00:55:02 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,13 +2,13 @@ require_relative "../config/environment.rb"
 class Student
   attr_accessor :name, :grade
   attr_reader :id
-  
+
   def initialize(name, grade, id=nil)
     @name = name
     @grade = grade
     @id = id
   end
-  
+
   def self.create_table
     sql = <<-SQL
     CREATE TABLE IF NOT EXISTS students (
@@ -19,12 +19,12 @@ class Student
     SQL
     DB[:conn].execute(sql)
   end
-  
+
   def self.drop_table
     sql = "DROP TABLE IF EXISTS students"
     DB[:conn].execute(sql)
   end
-  
+
   def save
     if self.id
       self.update
@@ -34,7 +34,7 @@ class Student
       @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
     end
   end
-  
+
   def self.create(name, grade)
     student = self.new(name, grade)
     student.save
@@ -49,14 +49,14 @@ class Student
   def self.new_from_db(row)
     self.new(row[1], row[2], row[0])
   end
-  
+
   def self.find_by_name(name)
     sql = "SELECT * FROM students WHERE name = ? LIMIT 1"
     DB[:conn].execute(sql, name).map {|row| self.new_from_db(row)}.first
   end
 
   # def self.find_by_name(name)
-  # 
+  #
   #   DB[:conn].execute(sql, name).map do |row|
   #     self.new_from_db(row)
   #   end.first
